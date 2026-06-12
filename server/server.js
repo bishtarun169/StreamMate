9e2835d94825628a640c5939e377eb6adbcf2102
Nishant Kumar <226134734+nish12965@users.noreply.github.com>
2026-06-12 17:48:25 +0530

    Socket Backend Added With fix

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const http = require('http');
 
 // env file check
 if (!process.env.JWT_SECRET) {
@@ -13,7 +14,22 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const initializeSocket = require("./src/socket");
+const server = http.createServer(app);
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
 });
 
+// make io available to controllers
+app.set("io", io);
+
+// initialize all socket handlers
+initializeSocket(io);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
