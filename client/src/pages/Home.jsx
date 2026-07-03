1695a621bb91f10787bcdc4e9fcd8c8fa97f8db6
Arun singh <231826282+bishtarun169@users.noreply.github.com>
2026-07-03 18:13:43 +0530

    created joinRoom page and minor udation in other files

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,9 @@ import { Outlet } from "react-router-dom";
 import DashboardHeader from "../components/home/AppNavbar";
 import Footer from "../components/landing/Footer";
 import DashboardBody from "../components/home/DashboardBody";
+import CreateRoomBody from "../components/createRoom/CreateRoomBody";
+import JoinRoom from "./JoinRoom";
+
 
 export default function Home() {
   return (
--- client/src/pages/JoinRoom.jsx
+++ client/src/pages/JoinRoom.jsx
@@ -1,14 +1,15 @@
-import CreateRoomBody from "../components/createRoom/CreateRoomBody";
+import JoinRoomBody from "../components/JoinRoom/JoinRoomBody";
+import Footer from "../components/landing/Footer";
 import useThemeInfo from "../hooks/useThemeInfo";
 
-export default function CreateRoom() {
+export default function JoinRoom() {
   const { theme } = useThemeInfo();
   const isDark = theme === "dark";
 
   return (
     <div className={`flex min-h-screen flex-col transition-colors duration-300 ${isDark ? "bg-[#0B0B0B] text-white" : "bg-slate-50 text-gray-900"}`}>
       <div className="flex-1">
-        <CreateRoomBody />
+        <JoinRoomBody />
       </div>
     </div>
   );
